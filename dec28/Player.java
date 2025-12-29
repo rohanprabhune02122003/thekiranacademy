53db8e7b42fe9c8e7b4d21fd46c6240ae20e3a34
rohanprabhune02122003 <rohanprabhune31@gmail.com>
2025-12-29 16:03:16 +0530

    Add files via upload

---
+++ dec28/Player.java
@@ -0,0 +1,30 @@
+package com.tka.dec28;
+
+public class Player {
+	
+	int jersey_no;
+	String p_name;
+	int runs;
+	int wickets;
+	String team_name;
+	
+	Player(int j, String p, int r, int w, String t){
+		
+		jersey_no = j;
+		p_name = p;
+		runs = r;
+		wickets = w;
+		team_name = t;
+		
+	}
+	
+	void display() {
+		
+		System.out.println("Jersey no : " + jersey_no);
+		System.out.println("Player name : " + p_name);
+		System.out.println("Runs : " + runs);
+		System.out.println("Wickets : " + wickets);
+		System.out.println("Team name : " + team_name);
+		System.out.println();
+	}
+}
---
+++ dec28/TeamMI.java
@@ -0,0 +1,32 @@
+package com.tka.dec28;
+
+public class TeamMI {
+	
+	public static void main(String[] args) {
+		
+		Player p1  = new Player(45, "Rohit Sharma", 6211, 15, "MI");
+        Player p2  = new Player(93, "Jasprit Bumrah", 56, 145, "MI");
+        Player p3  = new Player(63, "Suryakumar Yadav", 3249, 2, "MI");
+        Player p4  = new Player(33, "Hardik Pandya", 2309, 53, "MI");
+        Player p5  = new Player(55, "Ishan Kishan", 2324, 0, "MI");
+        Player p6  = new Player(99, "Tilak Varma", 1150, 0, "MI");
+        Player p7  = new Player(17, "Kieron Pollard", 3412, 69, "MI");
+        Player p8  = new Player(12, "Krunal Pandya", 1326, 61, "MI");
+        Player p9  = new Player(18, "Rahul Chahar", 100, 56, "MI");
+        Player p10 = new Player(5, "Trent Boult", 58, 105, "MI");
+        Player p11 = new Player(19, "Lasith Malinga", 56, 170, "MI");
+        
+        p1.display();
+        p2.display();
+        p3.display();
+        p4.display();
+        p5.display();
+        p6.display();
+        p7.display();
+        p8.display();
+        p9.display();
+        p10.display();
+        p11.display();
+	}
+
+}
